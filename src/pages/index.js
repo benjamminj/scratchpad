e501a6365404d353ec5ecb94c5ec26aa9655835b
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-07-17 21:22:55 -0700

    Fix layout of homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { css } from 'emotion'
 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <Link to="/bubble-sort/">bubble sort</Link>
-    <Link to="/insertion-sort/">insertion sort</Link>
+    <ul className={css`list-style-type: none; padding: 0;`}>
+      <li>
+        <Link to="/bubble-sort/">bubble sort</Link>
+      </li>
+      <li>
+        <Link to="/insertion-sort/">insertion sort</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
