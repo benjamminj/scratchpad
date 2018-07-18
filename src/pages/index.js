2b39b2c20cbbd5c8072ad1b5b7165a64b6b45180
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-07-17 21:17:46 -0700

    Add deploy script & new link from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <Link to="/bubble-sort/">bubble sort</Link>
+    <Link to="/insertion-sort/">insertion sort</Link>
   </Layout>
 )
 
